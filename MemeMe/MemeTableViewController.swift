e7c05f5a7623d2886d0cce269d9b64f9ffa17176
Adland Lee <adland@4mfd.com>
2016-02-22 13:57:30 -0800

    add Meme Table View

---
+++ MemeMe/MemeTableViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MemeTableViewController.swift
+//  MemeMe
+//
+//  Created by Adland Lee on 2/21/16.
+//  Copyright © 2016 Adland Lee. All rights reserved.
+//
+
+import UIKit
+
+class MemeTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    var memes: [Meme] {
+        return (UIApplication.sharedApplication().delegate as! AppDelegate).memes
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        (self.view as! UITableView).reloadData()
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.memes.count
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cellIdentifier = "MemeCell"
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier)!
+        let meme = self.memes[indexPath.row]
+        
+        // Set the name and image
+        cell.imageView?.image = meme.memedImage
+        cell.textLabel?.text = "\(meme.topText) \(meme.bottomText)"
+        
+        return cell
+    }
+}
